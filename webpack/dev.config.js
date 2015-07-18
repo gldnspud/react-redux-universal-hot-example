f04afe0e998eb2b5cc2da6ea6ad9ad56db8c98ed
Matthew Scott <matt@11craft.com>
2015-07-18 16:13:34 -0700

    Support non-localhost URLs for hot loader

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -13,7 +13,7 @@ module.exports = {
   context: path.resolve(__dirname, '..'),
   entry: {
     'main': [
-      'webpack-dev-server/client?http://' + host + ':' + port,
+      'webpack-dev-server/client?http://' + publicPathHost + ':' + port,
       'webpack/hot/only-dev-server',
       './src/client.js'
     ]
